ed3f1918b181c473b3be257d46d1fac8b1d26b74
정다인 <wjdekdls04@naver.com>
2024-11-06 17:57:52 +0900

    git

--- codingTest/5644무선충전/5644.py
+++ codingTest/5644무선충전/5644.py
@@ -9,7 +9,25 @@ for tc in range(1,2):
     B_arr = [0] + list(map(int, input().split()))
     Ar, Ac = 1, 1
     Br, Bc = 10, 10
-    AP = [list(map(int, input().split())) for _ in range(A)]
+    arr = [[0] * 10 for _ in range(10)]
+    for line in arr :
+        print(line)
+    for k in range(A) :
+        BCc, BCr, BCrange, BCpower = map(int, input().split())
+
+        for r in range(-BCrange, BCrange+1):
+            for c in range(-(BCrange-abs(r)), BCrange-abs(r)+1):
+                nr = BCr + r -1
+                nc = BCc + c -1
+                # print(k,'번', nr, nc)
+                if 0<=nr<10 and 0<=nc<10 :
+                    BC = str(k)+'번'
+                    arr[nr][nc]=(BC)
+        
+    print() 
+    for line in arr :
+        print(*line)
+
 
     DIRECTION = [(0,0), [-1,0], [0,1], [1,0], [0,-1]] # 이동하지 않음, 상, 우, 하, 좌
     
@@ -24,13 +42,11 @@ for tc in range(1,2):
         Bc += dBc
 
         for bc in range(A):
-            BCc = AP[bc][0]
-            BCr = AP[bc][1]
-            BCrange = AP[bc][2]
-            BCpower = AP[bc][3]
+            pass
 
             if abs(Ar-BCr) + abs(Ac-BCc) <= BCrange:
-                print(time, Ar, Ac)
+                # print(time, Ar, Ac)
+                pass
 
             if abs(Br-BCr) + abs(Bc-BCc) <= BCrange:
-                
+                pass
