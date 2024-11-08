ff73d648337159f6dd7a0310b4d18570ef37d291
정다인 <wjdekdls04@naver.com>
2024-11-08 17:51:25 +0900

    git

--- codingTest/5644무선충전/5644_2.py
+++ codingTest/5644무선충전/5644_2.py
@@ -2,7 +2,7 @@ import sys; sys.stdin=open('sample_input.txt')
 
 T = int(input())
 
-for tc in range(1,2):
+for tc in range(1,T+1):
     M, A = map(int, input().split())
 
     A_arr = [0] + list(map(int, input().split()))
@@ -14,6 +14,7 @@ for tc in range(1,2):
 
     DIRECTION = [(0,0), [-1,0], [0,1], [1,0], [0,-1]] # 이동하지 않음, 상, 우, 하, 좌
     
+    # print(BC)
     mx_sum = 0
     for time in range(M+1) :
         dAr, dAc = DIRECTION[A_arr[time]]
@@ -34,20 +35,12 @@ for tc in range(1,2):
             BCr -= 1
 
             if abs(Ar-BCr) + abs(Ac-BCc) <= BCrange:
-                if abs(Br-BCr) + abs(Bc-BCc) <= BCrange:
-                    A_cnt.append(battery)
-                    B_cnt.append(battery)
-
-                else :
-                    A_cnt.append(battery)
-
-            elif abs(Br-BCr) + abs(Bc-BCc) <= BCrange:
+                A_cnt.append(battery)
+            if abs(Br-BCr) + abs(Bc-BCc) <= BCrange:
                 B_cnt.append(battery)
 
-        a_mx = 0
-        b_mx = 0
-        a_bc=0
-        b_bc=0
+
+        ab_mx = 0
         if A_cnt or B_cnt :
             if A_cnt and B_cnt:
                 for a in A_cnt:
@@ -56,34 +49,37 @@ for tc in range(1,2):
                         b_bc = BC[b][3]
                         if a==b:
                             a_bc = int(a_bc/2)
-                            if a_mx < a_bc:
-                                a_mx = a_bc
                             b_bc = int(b_bc/2)
-                            if b_mx < b_bc:
-                                b_mx = b_bc
+                            ab_sum = a_bc + b_bc
+                            if ab_mx < ab_sum :
+                                ab_mx = ab_sum
                         else:
                             a_bc = BC[a][3]
-                            if a_mx < a_bc:
-                                a_mx = a_bc
                             b_bc = BC[b][3]
-                            if b_mx < b_bc:
-                                b_mx = b_bc
+                            ab_sum = a_bc + b_bc
+                            if ab_mx < ab_sum :
+                                ab_mx = ab_sum
+
 
             
             elif A_cnt :
                 for a in A_cnt:
                     a_bc = BC[a][3]
-                    if a_mx < a_bc:
-                        a_mx = a_bc
+                    ab_sum = a_bc
+                    if ab_mx < ab_sum :
+                        ab_mx = ab_sum
+
 
 
             elif B_cnt:
                 for b in B_cnt:
                     b_bc = BC[b][3]
-                    if b_mx < b_bc:
-                        b_mx = b_bc
+                    ab_sum = b_bc
+                    if ab_mx < ab_sum :
+                        ab_mx = ab_sum
 
-        mx_sum += a_mx + b_mx
-        print(time, a_mx, b_mx)
+
+        mx_sum += ab_mx
+        # print(time, ab_mx)
 
     print(f'#{tc}', mx_sum)
